a827426ebb0fde414d69d0f6448d6be68264b939
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-05-10 03:50:37 +0600

    hide pass

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 3000
@@ -6,12 +7,12 @@ const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json())
 
-//sifatesadakin10
-// zV7qKwpwaJz3I16i
+
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://sifatesadakin10:zV7qKwpwaJz3I16i@cluster0.rohhp7w.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rohhp7w.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
